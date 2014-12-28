bcbbdada7e77e175a580362dba37b3ddbb8403b9
Ormir G <ormirgj@gmail.com>
2014-12-28 13:30:09 +0100

    Login UI Update
    Added Header

--- SJ_App/StockJock/app/src/main/java/com/ogs/stockjock/LoginActivity.java
+++ SJ_App/StockJock/app/src/main/java/com/ogs/stockjock/LoginActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -35,5 +36,9 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_LogIn(View v){
+        // TODO Login the user
     }
 }
